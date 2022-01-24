4b1ce7cde4e70bacd63a83599fb554322672906f
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-01-24 18:25:22 +0000

    Big fixing

--- jobsite/src/App.js
+++ jobsite/src/App.js
@@ -1,22 +1,45 @@
-import './App.css';
 import NavBar from "./components/NavBar"
 import JobList from "./components/JobList"
 import { BrowserRouter as Router, Route, Routes, Redirect } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from "react"
 
 const App = () => {
-  return (
-   <div className="App">
-     <Router>
-              <NavBar />
-              <Routes>
-                <Route path="/home" exact component={JobList} />
-              
-                
-              </Routes>
-     </Router>
-   </div>
 
+  const [jobs, setJobs] = useState([])
+
+  const fetchAllJobs = async () => {
+
+    try {
+      let allJobs = await fetch(`https://strive-jobs-api.herokuapp.com/jobs?limit=50`)
+      if (allJobs.ok) {
+        let jobs = await allJobs.json()
+        setJobs(jobs)
+        console.log(jobs)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchAllJobs()
+  }, [])
+
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Routes>
+          <Route path="/" element={<JobList />} />
+
+          </Routes>
+        </div>
+
+
+      </div>
+    </Router>
   );
 }
 
--- jobsite/src/components/JobList.jsx
+++ jobsite/src/components/JobList.jsx
@@ -1,30 +1,11 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import SingleJob from "./SingleJob"
 
-const JobList = () => {
-  const [ jobs, setJobs ] = useState([])
-
-  const fetchAllJobs = async () => {
-      
-      try {
-          let allJobs = await fetch(`https://strive-jobs-api.herokuapp.com/jobs?limit=50`)
-          if (allJobs.ok) {
-              let jobs = await allJobs.json()
-              setJobs(jobs)
-          }
-      } catch (error) {
-          console.log(error)
-      }
-  }  
-
-  useEffect(() => {
-    fetchAllJobs()
-  }, [])
-
+const JobList = ({ jobs, fetchAllJobs }) => {
   return (
-  <>
-  <div>f\sdf\sdf\sdfhjfgdfgjdghjdghjfgj</div>
-  </>
+    <div>
+      {jobs?.map((element) => (<SingleJob fetchAllJobs={fetchAllJobs} jobs={jobs} element={element} key={element._id} />))}
+    </div>
   )
 };
 
